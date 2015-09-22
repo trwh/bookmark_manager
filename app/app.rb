721b2a89f3dfdc8ece22c7d499fbba5f21a0cbc2
TJQKAs <a26052013@gmail.com>
2015-09-22 16:29:40 +0100

    update all

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,14 @@ require 'sinatra/base'
 require_relative '../data_mapper_setup'
 
 class BookmarksWeb < Sinatra::Base
-  get '/' do
-    'Hello BookmarksWeb!'
-  end
+  get '/links'  do
+    @links = Link.all
+    p @links
+    erb :links
+end
 
+  set :views, proc { File.join(root, 'views') }
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
