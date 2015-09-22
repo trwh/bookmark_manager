96fc83b043adaf7707c67540881a5a8816b099fd
Tim Harrison <mail@trwh.co.uk>
2015-09-22 14:27:30 +0100

    Got the tests to fail sensibly.

--- app/app.rb
+++ app/app.rb
@@ -1,15 +1,10 @@
 require 'sinatra/base'
-require_relative 'models/link'
 
 class BookmarksWeb < Sinatra::Base
+  get '/' do
+    'Hello BookmarksWeb!'
+  end
 
-  # get '/links' do
-  #   @links = Link.all
-  #   erb :links
-  # end
-
-# start the server if ruby file executed directly
+  # start the server if ruby file executed directly
   run! if app_file == $0
-
-  set :views, proc { File.join(root, 'views') }
 end
