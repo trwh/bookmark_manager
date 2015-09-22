450953f4ba681065a35ce698aafccbdaa6211f58
Tim Harrison <mail@trwh.co.uk>
2015-09-22 16:46:14 +0100

    Added facility to add links

--- app/app.rb
+++ app/app.rb
@@ -2,11 +2,21 @@ require 'sinatra/base'
 require_relative '../data_mapper_setup'
 
 class BookmarksWeb < Sinatra::Base
-  get '/links'  do
+
+  get '/links' do
     @links = Link.all
     p @links
     erb :links
-end
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title] )
+    redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :links_new
+  end
 
   set :views, proc { File.join(root, 'views') }
   # start the server if ruby file executed directly
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -7,6 +7,7 @@ require 'rspec'
 feature 'Viewing links' do
 
   scenario 'I can see existing links on the links page' do
+
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
@@ -15,5 +16,22 @@ feature 'Viewing links' do
     end
   end
 
+end
+
+feature 'Creating links' do
+
+  scenario 'I can create a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.zombo.com/'
+    fill_in 'title', with: 'This is Zombocom'
+    click_button 'Create link'
+
+    # we expect to be redirected back to the links page
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('This is Zombocom')
+    end
+  end
 
 end
